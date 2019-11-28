4bbb880763b69b24162e9682b5e498c9447ad15f
Debbie <debiurquhart@live.co.uk>
2019-11-28 15:25:08 +0000

    adds bucketlist database file

---
+++ bucket_list/db/bucket_list.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS attractions;
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS countries;
+DROP TABLE IF EXISTS continents;
+
+CREATE TABLE continents(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE countries(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  continent_id INT REFERENCES continents(id)
+);
+
+CREATE TABLE cities(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  visit BOOLEAN,
+  country_id INT REFERENCES countries(id)
+);
+
+CREATE TABLE attractions(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  review TEXT,
+  review_rating INT,
+  city_id INT REFERENCES cities(id) ON DELETE CASCADE
+);
