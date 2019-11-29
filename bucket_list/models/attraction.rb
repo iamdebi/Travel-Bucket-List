5e010484c43348f9bfd2f455b0d5f825088b14d1
Debbie <debiurquhart@live.co.uk>
2019-11-29 12:31:08 +0000

    adds view functions to attraction class

--- bucket_list/models/attraction.rb
+++ bucket_list/models/attraction.rb
@@ -27,4 +27,32 @@ class Attraction
     SqlRunner.run(sql)
   end
 
+  def update()
+    sql = "UPDATE attractions SET
+    (name, review, review_rating, city_id) =
+    ($1, $2, $3, $4) WHERE id = $5 "
+    values = [@name, @review, @review_rating, @city_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM attractions
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.veiw_all()
+    sql = "SELECT * FROM attractions"
+    attractions = SqlRunner.run(sql)
+    return attractions.map{|attraction| Attraction.new(attraction)}
+  end
+
+  def self.view(id)
+    sql ="SELECT * FROM attractions WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    return result.first
+  end
+
 end
