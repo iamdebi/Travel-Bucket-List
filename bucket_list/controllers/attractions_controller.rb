5297836fe0744565efb4bfb486c1bb2314e9c135
Debbie <debiurquhart@live.co.uk>
2019-11-30 22:56:09 +0000

    create attraction erb files

--- bucket_list/controllers/attractions_controller.rb
+++ bucket_list/controllers/attractions_controller.rb
@@ -3,3 +3,42 @@ require('sinatra/contrib/all')
 
 require_relative('../models/attraction')
 also_reload( '../models/*' )
+
+#index
+get '/attractions' do
+
+end
+
+# #new
+get '/attractions/new' do
+
+end
+
+#show
+get '/attractions/:id' do
+  id = params['id'].to_i
+end
+
+# create
+post '/attractions' do
+
+  redirect'/attractions'
+end
+
+# edit
+get '/attractions/:id/edit' do
+
+end
+
+# update
+post '/attractions/:id' do
+  
+  redirect '/attractions'
+
+end
+
+# delete
+post '/attractions/:id/delete' do
+
+  redirect '/atractions'
+end
